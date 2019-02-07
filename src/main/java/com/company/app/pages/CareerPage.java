4a718c5eff418eb07924169baa38fb282edb46c9
Navedanjum Ansari <ansari@ut.ee>
2019-02-07 08:00:52 +0200

    Added career page tests and mapped object

--- src/main/java/com/company/app/pages/CareerPage.java
+++ src/main/java/com/company/app/pages/CareerPage.java
@@ -1,11 +1,65 @@
 package com.company.app.pages;
 
 import com.company.app.base.CommonWebElements;
-import com.company.app.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 /**
- * @author Ansari on 2/6/2019
+ * @author Ansari on 2/7/2019
  */
 public class CareerPage extends CommonWebElements {
+
+    @FindBy(css = "div.slide-subtitle.p")
+    WebElement careerIntroSection;
+
+    @FindBy(xpath = "//div[@data-id='13d2581']")
+    WebElement currentOpeningSection;
+
+    @FindBy(xpath = "//div[@data-id='4560809']")
+    WebElement uniqueSection;
+
+    @FindBy(xpath = "//div[@data-id='b8dabfb']")
+    WebElement workEnvironmentSection;
+
+    @FindBy(xpath = "//div[@data-id='36e7fb4']")
+    WebElement benefitSection;
+
+    @FindBy(xpath = "//div[@data-id='2cf5596']")
+    WebElement contactSection;
+
+    @FindBy(xpath = "//span[contains(text(),'Team Page')]")
+    WebElement teamPageBtn;
+
+    @FindBy(xpath = "//span[contains(text(),'Drop Us a Line')]")
+    WebElement dropUsBtn;
+
+    @FindBy(xpath = "//span[@class='job-name']")
+    List<WebElement> jobList;
+
+    public String getCareerIntroSectionHeaderText() {
+        return careerIntroSection.getText();
+    }
+
+    public String getCurrentOpeningSectionHeaderText() {
+        return currentOpeningSection.getText();
+    }
+
+    public String getUniqueSectionHeaderText() {
+        return uniqueSection.getText();
+    }
+
+    public String getWorkEnvironmentSectionHeaderText() {
+        return workEnvironmentSection.getText();
+    }
+
+    public String getBenefitSectionHeaderText() {
+        return benefitSection.getText();
+    }
+
+    public String getContactSectionHeaderText() {
+        return contactSection.getText();
+    }
 
 }
--- src/test/java/com/company/app/testcases/CareerPageTests.java
+++ src/test/java/com/company/app/testcases/CareerPageTests.java
@@ -1,11 +1,12 @@
 package com.company.app.testcases;
 
-import com.company.app.base.CommonWebElements;
 import com.company.app.base.TestBase;
 import com.company.app.pages.CareerPage;
 import com.company.app.pages.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  * @author Ansari on 2/6/2019
@@ -25,6 +26,29 @@ public class CareerPageTests extends TestBase {
         webDriverInitialization();
         homePage = new HomePage();
         careerPage = homePage.clickCareerTab();
+    }
+
+    @Test(priority=1)
+    public void verifyCareerContentSectionTest(){
+
+        String careerIntroHeader = "CAREER AT IT DEVELOPMENT AND INNOVATION CENTER";
+        Assert.assertEquals(careerPage.getCareerIntroSectionHeaderText(),careerIntroHeader);
+
+        String careerOpeningsHeader = "Current openings";
+        Assert.assertEquals(careerPage.getCurrentOpeningSectionHeaderText(),careerOpeningsHeader);
+
+        String uniqueHeader = "What makes us unique";
+        Assert.assertEquals(careerPage.getUniqueSectionHeaderText(),uniqueHeader);
+
+        String workEnvHeader = "Modern work environment";
+        Assert.assertEquals(careerPage.getWorkEnvironmentSectionHeaderText(),workEnvHeader);
+
+        String benefitHeader = "Benefits";
+        Assert.assertEquals(careerPage.getBenefitSectionHeaderText(),benefitHeader);
+
+        String contactHeader = "Don’t see the position you’re looking for? \nWe’d still love to hear from you!";
+        Assert.assertEquals(careerPage.getContactSectionHeaderText(),contactHeader);
+
     }
 
 
